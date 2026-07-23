8b84943a3b3dece417afe8484b167ef8eaa1b928
shiveshkumar7007 <shiveshkumarid@gmail.com>
2026-07-24 01:05:37 +0530

    deployment bugs fixed

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,18 +1,20 @@
 import generateToken from "./generateToken.js";
 
 const sendToken = (user, statusCode, message, res) => {
+  // Call your standalone helper function passing the user id
   const token = generateToken(user._id);
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Keeps it secure on Render/Vercel (HTTPS)
-    sameSite: "none", // Required for cross-site cookies between Vercel and Render
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.status(statusCode).json({
     success: true,
     message,
+    token,
     user: {
       id: user._id,
       name: user.name,
